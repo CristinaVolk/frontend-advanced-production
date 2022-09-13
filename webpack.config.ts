4c4ae4dcfa83f102a8073ee016695410ab2f157a
cristinavolk <kvolk@griddynamics.com>
2022-09-13 14:57:18 +0200

    2.3 Webpack-dev-server.Environment vars

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,21 +2,27 @@ import path from 'path'
 import webpack from 'webpack'
 
 import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
-import {BuildPaths} from "./config/build/types/optionsConfig";
+import {BuildEnvVar, BuildPaths} from "./config/build/types/optionsConfig";
 
-export const paths: BuildPaths = {
-	entry: path.resolve(__dirname, 'src', 'index'),
-	build: path.resolve(__dirname, 'build'),
-	html: path.resolve(__dirname, 'public', 'index.html')
+
+export default (env: BuildEnvVar) => {
+	const mode = env.mode || 'development';
+	const port = env.port || 3000;
+	const isDev = mode === 'development';
+
+	const paths: BuildPaths = {
+		entry: path.resolve(__dirname, 'src', 'index'),
+		build: path.resolve(__dirname, 'build'),
+		html: path.resolve(__dirname, 'public', 'index.html')
+	}
+
+	const config: webpack.Configuration = buildWebpackConfig({
+		mode,
+		paths,
+		isDev,
+		port
+	})
+
+	return config
 }
 
-const mode = 'development'
-const isDev = mode === 'development';
-
-const config: webpack.Configuration = buildWebpackConfig({
-	mode: 'development',
-	paths,
-	isDev
-})
-
-export default config;
