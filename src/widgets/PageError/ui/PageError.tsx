02747293b964a86059279a9ceb14660c774146d2
cristinavolk <kvolk@griddynamics.com>
2022-10-02 17:16:20 +0200

    PageError style [fixed]

--- src/widgets/PageError/ui/PageError.tsx
+++ src/widgets/PageError/ui/PageError.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 
 import { classNames } from 'shared/lib/classNames';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { ThemeEnum } from 'app/providers/ThemeProvider';
 import classes from './PageError.module.scss';
 
 interface PageErrorProps {
@@ -15,14 +16,17 @@ export const PageError = ({ className }: PageErrorProps) => {
     // eslint-disable-next-line no-restricted-globals
     location.reload();
   };
+
   return (
-       <div className={classNames(classes.PageError, {}, [className])}>
-            <Button
-               onClick={onRefresh}
-               theme={ThemeButton.CREATIVE}
-            >
-                 {t('refresh')}
-            </Button>
+       <div className={`app ${ThemeEnum.DARK}`}>
+            <div className={classNames(classes.PageError, {}, [className])}>
+                 <Button
+                    onClick={onRefresh}
+                    theme={ThemeButton.CREATIVE}
+                 >
+                      {t('refresh')}
+                 </Button>
+            </div>
        </div>
   );
 };
