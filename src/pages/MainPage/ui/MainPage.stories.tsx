9107eb818df123dcc70db72e8e6fcdea760791f1
cristinavolk <kvolk@griddynamics.com>
2022-10-05 11:33:50 +0200

    3.28 commands [update]

--- src/pages/MainPage/ui/MainPage.stories.tsx
+++ src/pages/MainPage/ui/MainPage.stories.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-
-import { ThemeDecorator } from 'shared/config/storybook/decorators/ThemeDecorator';
-import { ThemeEnum } from 'app/providers/ThemeProvider';
 import MainPage from './MainPage';
 
 export default {
@@ -19,7 +16,3 @@ const Template: ComponentStory<typeof MainPage> = (args) => (
 
 export const LightAboutPage = Template.bind({});
 LightAboutPage.args = {};
-
-export const DarkAboutPage = Template.bind({});
-DarkAboutPage.args = {};
-DarkAboutPage.decorators = [ThemeDecorator(ThemeEnum.DARK)];
