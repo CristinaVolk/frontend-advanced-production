72f9fef6072e6dd34de013974c0c94fd9eed5561
cristinavolk <kvolk@griddynamics.com>
2022-10-09 20:06:23 +0200

    3.30 loki modal fix

--- src/shared/ui/Modal/Modal.stories.tsx
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -11,7 +11,7 @@ export default {
     backgroundColor: { control: 'color' },
   },
   args: {
-    isOpen: true,
+    isOpen: false,
     children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
   },
 } as ComponentMeta<typeof Modal>;
@@ -20,7 +20,6 @@ const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
 
 export const LightModal = Template.bind({});
 LightModal.args = {
-  className: 'app',
 };
 LightModal.decorators = [ThemeDecorator(ThemeEnum.LIGHT)];
 
