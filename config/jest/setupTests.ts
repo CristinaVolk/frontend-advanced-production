4e003466303e3ba961d2b6ae7b6f5a8e7b9c99ed
cristinavolk <kvolk@griddynamics.com>
2023-01-04 00:33:48 +0100

    14.2 Fetch warning [fixed]

--- config/jest/setupTests.ts
+++ config/jest/setupTests.ts
@@ -1,8 +1,2 @@
 import '@testing-library/jest-dom/extend-expect';
 import 'regenerator-runtime/runtime';
-
-// const nodeFetch = require('node-fetch');
-//
-// // @ts-ignore
-// global.fetch = nodeFetch;
-// global.Request = nodeFetch.Request;
--- src/shared/api/rtkApi.ts
+++ src/shared/api/rtkApi.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import fetch from 'isomorphic-fetch';
 import { USER_LOCAL_STORAGE_KEY } from '@/shared/const/localStorage';
 
 // Define a service using a base URL
@@ -6,6 +7,7 @@ export const rtkApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
     baseUrl: __API__,
+    fetchFn: fetch,
     prepareHeaders: (headers) => {
       const token = localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '';
       if (token) {
